958b5a949f26c934897670744ce31f828700dcae
Nate <for.education.n@yandex.ru>
2021-08-26 21:24:26 +0200

    Added initial projects files and settings

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-app
+
+Weather app using HTML, CSS and Vanilla JS, webpack
+
+This project is a part of [The Odin Project](https://www.theodinproject.com/)'s curriculum.
+
+Live Demo: [Link]()
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+      favicon: './src/images/favicon.png',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+};
