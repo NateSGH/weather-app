264879a20fa888342b67ded7137379d1564bf16a
Nate <for.education.n@yandex.ru>
2021-08-28 21:22:31 +0200

    Added wether info from api, changed structure. Style WIP

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const unitSlider = document.getElementById('unit-slider');
 let units = 'metric';
 let currentWeatherCity = '';
 
+document.querySelector('body').style.background = "url('/src/images/background.jpg')";
+
 function capitilizeString(phrase) {
   return phrase.charAt(0).toUpperCase() + phrase.slice(1);
 }
@@ -18,11 +20,20 @@ function printWeatherData(weatherObj) {
   const container = document.querySelector('.weather-container');
   container.innerHTML = '';
 
+  const weatherIconWrapper = document.createElement('div');
+  weatherIconWrapper.classList.add('weather-icon-div');
+
+  const weatherIcon = document.createElement('img');
+  weatherIcon.src = `http://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@4x.png`;
+
+  const weatherInfoWrapper = document.createElement('div');
+  weatherInfoWrapper.classList.add('weather-info-div');
+
   const cityName = document.createElement('p');
   cityName.innerText = `${weatherObj.name}, ${weatherObj.sys.country}`;
 
   const currentDate = document.createElement('p');
-  currentDate.innerText = format(fromUnixTime(weatherObj.dt), 'dd.MM.yyyy');
+  currentDate.innerText = format(fromUnixTime(weatherObj.dt), 'iiii, do LLL yyyy kk:mm');
 
   const temperature = document.createElement('p');
   temperature.innerHTML = `${weatherObj.main.temp}<span>&#176;</span>${
@@ -37,11 +48,32 @@ function printWeatherData(weatherObj) {
     units === 'metric' ? 'C' : 'F'
   }`;
 
-  container.appendChild(cityName);
-  container.appendChild(currentDate);
-  container.appendChild(temperature);
-  container.appendChild(feelsTemp);
-  container.appendChild(weather);
+  const windSpeed = document.createElement('p');
+  windSpeed.innerText = `Wind: ${weatherObj.wind.speed} ${
+    units === 'metric' ? 'm/s' : 'miles/hour'
+  }`;
+
+  // Photo by Lum3n from Pexels
+
+  const sunriseTime = document.createElement('p');
+  sunriseTime.innerText = `Sunrise: ${format(fromUnixTime(weatherObj.sys.sunrise), 'kk:mm')}`;
+
+  const sunsetTime = document.createElement('p');
+  sunsetTime.innerText = `Sunset: ${format(fromUnixTime(weatherObj.sys.sunset), 'kk:mm')}`;
+
+  weatherIconWrapper.appendChild(weatherIcon);
+
+  weatherInfoWrapper.appendChild(cityName);
+  weatherInfoWrapper.appendChild(currentDate);
+  weatherInfoWrapper.appendChild(temperature);
+  weatherInfoWrapper.appendChild(feelsTemp);
+  weatherInfoWrapper.appendChild(weather);
+  weatherInfoWrapper.appendChild(windSpeed);
+  weatherInfoWrapper.appendChild(sunriseTime);
+  weatherInfoWrapper.appendChild(sunsetTime);
+
+  container.appendChild(weatherIconWrapper);
+  container.appendChild(weatherInfoWrapper);
 }
 
 async function getWeatherByCityName(city) {
