e492710af8eccdd952bd487c5dc09dac1802f9ad
Nate <for.education.n@yandex.ru>
2021-08-29 21:25:54 +0200

    Changed structure (html). New background. Style WIP

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ const unitSlider = document.getElementById('unit-slider');
 let units = 'metric';
 let currentWeatherCity = '';
 
-document.querySelector('body').style.background = "url('/src/images/background.jpg')";
+// Photo by Lua Morales from Pexels
+document.querySelector('html').style.background =
+  "url('/src/images/background.jpg') no-repeat center center fixed";
+
+document.querySelector('html').style.backgroundSize = 'cover';
 
 function capitilizeString(phrase) {
   return phrase.charAt(0).toUpperCase() + phrase.slice(1);
@@ -26,22 +30,31 @@ function printWeatherData(weatherObj) {
   const weatherIcon = document.createElement('img');
   weatherIcon.src = `http://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@4x.png`;
 
+  const weather = document.createElement('p');
+  weather.innerText = capitilizeString(weatherObj.weather[0].description);
+
   const weatherInfoWrapper = document.createElement('div');
   weatherInfoWrapper.classList.add('weather-info-div');
 
+  const weatherMainInfo = document.createElement('div');
+  weatherMainInfo.classList.add('weather-main-info');
+
   const cityName = document.createElement('p');
   cityName.innerText = `${weatherObj.name}, ${weatherObj.sys.country}`;
+  cityName.classList.add('city-name');
 
   const currentDate = document.createElement('p');
   currentDate.innerText = format(fromUnixTime(weatherObj.dt), 'iiii, do LLL yyyy kk:mm');
+  currentDate.classList.add('date');
 
   const temperature = document.createElement('p');
   temperature.innerHTML = `${weatherObj.main.temp}<span>&#176;</span>${
     units === 'metric' ? 'C' : 'F'
   }`;
+  temperature.classList.add('temperature');
 
-  const weather = document.createElement('p');
-  weather.innerText = capitilizeString(weatherObj.weather[0].description);
+  const weatherSideInfo = document.createElement('div');
+  weatherSideInfo.classList.add('weather-side-info');
 
   const feelsTemp = document.createElement('p');
   feelsTemp.innerHTML = `Feels Like: ${weatherObj.main.feels_like}<span>&#176;</span>${
@@ -53,8 +66,6 @@ function printWeatherData(weatherObj) {
     units === 'metric' ? 'm/s' : 'miles/hour'
   }`;
 
-  // Photo by Lum3n from Pexels
-
   const sunriseTime = document.createElement('p');
   sunriseTime.innerText = `Sunrise: ${format(fromUnixTime(weatherObj.sys.sunrise), 'kk:mm')}`;
 
@@ -62,15 +73,19 @@ function printWeatherData(weatherObj) {
   sunsetTime.innerText = `Sunset: ${format(fromUnixTime(weatherObj.sys.sunset), 'kk:mm')}`;
 
   weatherIconWrapper.appendChild(weatherIcon);
+  weatherIconWrapper.appendChild(weather);
 
-  weatherInfoWrapper.appendChild(cityName);
-  weatherInfoWrapper.appendChild(currentDate);
-  weatherInfoWrapper.appendChild(temperature);
-  weatherInfoWrapper.appendChild(feelsTemp);
-  weatherInfoWrapper.appendChild(weather);
-  weatherInfoWrapper.appendChild(windSpeed);
-  weatherInfoWrapper.appendChild(sunriseTime);
-  weatherInfoWrapper.appendChild(sunsetTime);
+  weatherMainInfo.appendChild(cityName);
+  weatherMainInfo.appendChild(currentDate);
+  weatherMainInfo.appendChild(temperature);
+
+  weatherSideInfo.appendChild(feelsTemp);
+  weatherSideInfo.appendChild(windSpeed);
+  weatherSideInfo.appendChild(sunriseTime);
+  weatherSideInfo.appendChild(sunsetTime);
+
+  weatherInfoWrapper.appendChild(weatherMainInfo);
+  weatherInfoWrapper.appendChild(weatherSideInfo);
 
   container.appendChild(weatherIconWrapper);
   container.appendChild(weatherInfoWrapper);
